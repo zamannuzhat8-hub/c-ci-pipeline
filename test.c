b32685c63f31bf23d2cac0b2c0d47ee7cff6b815
zamannuzhat8-hub <zamannuzhat8@gmail.com>
2025-12-15 05:04:37 +0600

    Update test.c

--- test.c
+++ test.c
@@ -1,1 +1,20 @@
+// test.c
+#include <stdio.h>
+#include <assert.h>
+
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    
+    assert(add(10, 20) == 30); 
+    
+    assert(add(5, 5) == 10);
+    
+    printf("All unit tests passed successfully!\n");
+    return 0;
+}
+
 
